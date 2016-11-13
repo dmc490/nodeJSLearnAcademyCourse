44ba42cdd2e2cc017b2bfdb58e1bdc9e1493176d
dmc490 <tzviru@gmail.com>
2016-11-13 22:27:51 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+/**
+ * Created by userr on 13/11/2016.
+ */
+var math = require('./math');
+var fs = require('fs');
+var _ = require('lodash');
+
+console.log(_.snakeCase("someValue"));
---
+++ math.js
@@ -0,0 +1,12 @@
+/**
+ * Created by userr on 13/11/2016.
+ */
+module.exports.add= function(a,b){
+    return a+b
+}
+module.exports.sub= function(a,b){
+    return a-b
+}
+module.exports.mul= function(a,b){
+    return a*b
+}
