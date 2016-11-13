c0ba87d5c75b68c4844e057d6554ae319eeab5ca
dmc490 <tzviru@gmail.com>
2016-11-13 22:47:31 +0200

    updated npm scripts and stuff

--- app.js
+++ app.js
@@ -4,5 +4,9 @@
 var math = require('./math');
 var fs = require('fs');
 var _ = require('lodash');
-
-console.log(_.snakeCase("someValue"));
+var axios = require('axios');
+axios.get("http://rest.learncode.academy/api/myuser/friends").then((res)=>{
+    var jake = _.find(res.data,{name:"Jake"})
+    console.log(jake)
+});
+//console.log(_.snakeCase("someValue"));
